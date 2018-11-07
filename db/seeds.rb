963f8ac6f12623b953f96fc28d57134edd6f99f5
Jasper Brumter <jasper.brumter@mail.mcgill.ca>
2018-11-07 14:14:22 -0500

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying all previous restaurants..."
+Restaurant.destroy_all
+
+puts "creating new restaurants..."
+puts "and adding reviews..."
+50.times do |variable|
+  @restaurant = Restaurant.create(
+{
+  name: Faker::Coffee.blend_name,
+  address: Faker::Coffee.origin,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+})
+
+5.times do
+  Review.create({
+    restaurant_id: @restaurant.id,
+    content: Faker::RickAndMorty.quote,
+    rating: rand(0..5)
+  })
+end
+end
